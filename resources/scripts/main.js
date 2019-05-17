d6243ed5622e33c1b9ed81ba97c21ea53a74618b
Zaheer <zaheer.mughal1965@gmail.com>
2019-05-17 22:29:07 +0500

    All front end stuff completed, only animation left

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -69,101 +69,59 @@ $('document').ready(function(){
 
 /************ Load more Projets
 ****************************************************/
-var project_item_one =  '<div class="project-item ltr">';
-    project_item_one += '<div class="project-image">';
-    project_item_one += '<img src="https://picsum.photos/520/320" alt="">';
-    project_item_one += '</div>';
-    project_item_one += '<h3 class="project-title">Project title</h3>';
-    project_item_one += '<div class="project-description">';
-    project_item_one += 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
-    project_item_one += '</div>';
-    project_item_one += '<ul class="project-technologies"><li>React</li><li>Laravel</li><li>Php</li><li>Css</li><li>Sass</li>  </ul>';
-    project_item_one += '<div class="project-links">'
-    project_item_one += '<a href="#"><svg><use href="#github-icon" /></svg></a>';
-    project_item_one += '<a href="#"><svg><use href="#external-link-icon" /></svg></a></div></div>';
-
-var project_item_two =  '<div class="project-item rtl">';
-    project_item_two += '<div class="project-image">';
-    project_item_two += '<img src="https://picsum.photos/520/320" alt="">';
-    project_item_two += '</div>';
-    project_item_two += '<h3 class="project-title">Project title</h3>';
-    project_item_two += '<div class="project-description">';
-    project_item_two += 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
-    project_item_two += '</div>';
-    project_item_two += '<ul class="project-technologies"><li>React</li><li>Laravel</li><li>Php</li><li>Css</li><li>Sass</li>  </ul>';
-    project_item_two += '<div class="project-links">'
-    project_item_two += '<a href="#"><svg><use href="#github-icon" /></svg></a>';
-    project_item_two += '<a href="#"><svg><use href="#external-link-icon" /></svg></a></div></div>';
-
-
-
-
-
-
-
-
-
-
-
-
 
 const LOAD_PROJECTS_BTN =  $('#loadMoreProjects');
 const LOADING_iCON = $('#loading_icon').hide();
 const PROJECTS_CONTAINER = $('.project-items-container');
-var currentProjectsCount = 2;
-var totalProjectsCount = null;
+var currentProjectsCount = 0;
+var totalProjectsCount = -1;
 var projectsData = null;    // we load data only once, this is to store cache.
 
+loadProjects();  // load 2 projects by default
 LOAD_PROJECTS_BTN.click(function(){
   // alert('load projects and append in project container');
-  loadProjects();
+  showLoading();
+  setTimeout(loadProjects,1000); //artificial loading
 });
 
 
   function loadProjects(){
-    showLoading();
+    // showLoading();
     // get the projects and append the projects
-
     // grab the projects, if success then append and hide loading
     // if fails or no more projects are there then disable loadProjects button
     //*******  /resources/scripts/data.json
-
     if(projectsData == null){
-      // console.log('Download and extract data');
       downloadData();
     }else{
       extractAndAppendProjects();
-      // console.log('Only extract data');
     }
-
-
-    // setTimeout(function(){
-    //   //
-    //   hideLoading();
-    //   PROJECTS_CONTAINER.append(project_item_one);
-    //   PROJECTS_CONTAINER.append(project_item_two);
+    // if(currentProjectsCount >= totalProjectsCount){
     //   disableLoadProjectButton();
-    // },1000);
+    //   // hideLoading();
+    // }else{
+    //
+    // }
 
   }
-  function isEven($number){
-    if($number==0) return true;
-    return $number/2 ? true : false;
+  function isEven(number){
+    // if(number==0) return true;
+    return (number%2 == 0) ? true : false;
   }
   function showLoading(){
     LOAD_PROJECTS_BTN.hide();
     LOADING_iCON.show();
-
   }
   function hideLoading(){
     LOADING_iCON.hide();
     LOAD_PROJECTS_BTN.show();
   }
   function disableLoadProjectButton(){
-    // LOAD_PROJECTS_BTN.attr('disabled','true');
-    // LOAD_PROJECTS_BTN.text('There are no more projects');
+    LOAD_PROJECTS_BTN.attr('disabled','true');
+    LOAD_PROJECTS_BTN.text('There are no more projects');
+    hideLoading();
     //or
-    LOAD_PROJECTS_BTN.hide();
+    // LOAD_PROJECTS_BTN.hide();
   }
   function downloadData(){
     var url = '/resources/scripts/data.json';
@@ -171,10 +129,6 @@ LOAD_PROJECTS_BTN.click(function(){
         projectsData = data;
         totalProjectsCount = projectsData.projects.length;
         extractAndAppendProjects();
-        // console.log(data.projects[0].projectTitle);
-        console.log(totalProjectsCount);
-        // console.log("successfull");
-        console.log(data);
     }
     var onFailure = function (jqXHR, textStatus, error) {
       console.log("Error occur");
@@ -191,22 +145,51 @@ LOAD_PROJECTS_BTN.click(function(){
       error: onFailure
     });
   }
-  function appendProjectItem(projectItem){
-    console.log('apend project');
-    console.log(projectItem);
+  function appendProjectItem(projectItem, className){
+    if(projectItem){
+        var project_item_html =  '<div class="project-item '+ className +'">';
+            project_item_html += '<div class="project-image">';
+            project_item_html += '<img src="'+projectItem.image+'" alt="">';
+            project_item_html += '</div>';
+            project_item_html += '<h3 class="project-title">'+projectItem.title+'</h3>';
+            project_item_html += '<div class="project-description">';
+            project_item_html += projectItem.description;
+            project_item_html += '</div>';
+            var projectsTechnologies = '';
+            for(var i=0; i<projectItem.technologies.length; i++){
+              projectsTechnologies += "<li>"+projectItem.technologies[i]+"</li>";
+            }
+            project_item_html += '<ul class="project-technologies">'+projectsTechnologies+' </ul>';
+            project_item_html += '<div class="project-links">'
+            project_item_html += '<a href="'+projectItem.githubLink+'"><svg><use href="#github-icon" /></svg></a>';
+            project_item_html += '<a href="'+projectItem.liveLink+'"><svg><use href="#external-link-icon" /></svg></a></div></div>';
+            PROJECTS_CONTAINER.append(project_item_html);
+          }
   }
+
   function extractAndAppendProjects(){
+        // console.log("CurrentCount: "+currentProjectsCount);
+        // console.log("TotalCount: "+totalProjectsCount);
+        if(currentProjectsCount < totalProjectsCount){
+              var loadCount = currentProjectsCount + 2 ;   // we only want to load 2 projects at one request
+              var projectItem = '';
+              //append projects
+              for(currentProjectsCount; currentProjectsCount < loadCount; currentProjectsCount++)
+              {
+                projectItem = projectsData.projects[currentProjectsCount];
 
-    var loadCount = currentProjectsCount + 2 ;   // we only want to load 2 projects at one request
-    var projectItem = '';
-    for(currentProjectsCount; currentProjectsCount < loadCount; currentProjectsCount++)
-    {
-      console.log(projectsData.projects[currentProjectsCount]);
-      projectItem = projectsData.projects[currentProjectsCount];
-      appendProjectItem(projectItem);
-    }
-
-    // appen project
-    hideLoading();
+                if(isEven(currentProjectsCount)){
+                  console.log(currentProjectsCount+' is Even');
+                  appendProjectItem(projectItem, 'ltr');
+                }else{
+                  console.log(currentProjectsCount+' is Odd');
+                  appendProjectItem(projectItem, 'rtl');
+                }
+                // console.log(projectsData.projects[currentProjectsCount]);
+              }
+              hideLoading();
+        }else{
+          disableLoadProjectButton();
+        }
   }
 });
