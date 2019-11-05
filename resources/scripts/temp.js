1c91270f69cc97633e77643b26df83b062776449
Zaheer <zaheer.mughal1965@gmail.com>
2019-11-05 23:40:08 +0500

    Discussion form hosting expired, so removed & ifood project link fixed

---
+++ resources/scripts/temp.js
@@ -0,0 +1,13 @@
+
+/*{
+      "image": "https://shahzaibzaheer.com/resources/screenshorts/discussion_forum.png",
+      "title": "Discussion Forum",
+      "description": "This is complete discussion form, in which user can create new discussions, can receive replies and user can also mark reply as best as well as like any reply",
+      "technologies": [
+        "Laravel",
+        "Bootstrap"
+      ],
+      "githubLink": "https://github.com/shahzaibzaheer/DiscussionForum",
+      "liveLink": "http://laravel-discussion-forum.tk"
+    }
+ */
